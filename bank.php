09314801662b83a327caef869f721db17fdfdd81
Md. Al-Amin <alamin2001islam@gmail.com>
2024-06-04 11:16:13 +0600

    Initial commit

---
+++ bank.php
@@ -0,0 +1,17 @@
+<?php
+ trait Bank{
+public function abc(){
+
+    echo "Its comes from abc function";
+
+ }
+
+ public function efg(){
+
+    echo "Its comes from efg function";
+
+ }
+
+ }
+
+?>
---
+++ common.php
@@ -0,0 +1,16 @@
+<?php
+ require "bank.php";
+
+ class Bar
+ {
+    use Bank;
+ }
+
+ $object = new Bar;
+ 
+ $object->abc();
+ $object ->efg();
+
+
+
+?>
